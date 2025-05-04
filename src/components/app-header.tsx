817c28b90290b94a55b8c7e9c91eddc08d08f3a0
Torben Haack <t128n@ipv4.8shield.net>
2025-05-04 18:48:17 +0200

    feat: add dark-theme toggle

--- src/components/app-header.tsx
+++ src/components/app-header.tsx
@@ -1,3 +1,4 @@
+import { ThemeToggle } from "@/components/theme-toggle";
 import { SplitIcon } from "lucide-react";
 
 function AppHeader() {
@@ -7,6 +8,9 @@ function AppHeader() {
 				<div className="flex items-center gap-2">
 					<SplitIcon className="w-4 h-4" />
 					<h1 className="text-xl font-semibold">routr</h1>
+				</div>
+				<div className="flex items-center">
+					<ThemeToggle />
 				</div>
 			</div>
 		</header>
