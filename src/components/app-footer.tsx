f435b18584f3fb9a89dc92a4d836a1b4b2da8870
Torben Haack <tohaack@pm.me>
2025-05-04 23:51:10 +0200

    fix: tweak footer

--- src/components/app-footer.tsx
+++ src/components/app-footer.tsx
@@ -1,5 +1,3 @@
-import { GitCommitVerticalIcon } from "lucide-react";
-
 function AppFooter() {
 	return (
 		<footer className="p-4 border-t border-dashed">
@@ -10,29 +8,15 @@ function AppFooter() {
 							href="https://t128n.github.io/"
 							className="flex items-center gap-2"
 						>
-							<img
-								src="https://t128n.github.io/favicon.svg"
-								alt="t128n"
-								className="w-6 h-6 rounded-full"
-							/>
 							<span>&copy; 2025 Torben Haack</span>
-							<span>&lt;t128n@ipv4.8shield.net&gt;</span>
+							<span className="hidden md:inline">&lt;t128n@ipv4.8shield.net&gt;</span>
 						</a>
 					</p>
 				</div>
-				<a
-					href="https://github.com/t128n/routr"
-					target="_blank"
-					rel="noreferrer"
-					className="flex items-center gap-2 text-sm opacity-25"
-				>
-					<GitCommitVerticalIcon className="w-4 h-4" />
-					<span>Link to GitHub Repository</span>
-				</a>
 				<div className="flex items-center">
 					<p className="text-sm flex items-center gap-1">
 						<span>v{import.meta.env.PACKAGE_VERSION}</span>
-						<span>&lt;{import.meta.env.BUILD_TIME}&gt;</span>
+						<span className="hidden md:inline">&lt;{import.meta.env.BUILD_TIME}&gt;</span>
 					</p>
 				</div>
 			</div>
